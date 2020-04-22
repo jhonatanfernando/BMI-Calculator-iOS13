ec7707d6db68ca56ec98c03b2a38ce3e5dce063a
Jhonatan Fernando <jhonatanfernando@yahoo.com.br>
2020-04-21 20:46:52 -0400

    WIP: Creating second controller

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func sliderHeightChanged(_ sender: UISlider) {
+        let height =  String(format:"%.2f", sender.value)
+        heightLabel.text = "\(height)m"
+        
+    }
+    
+    
+    @IBAction func sliderWeigthChanged(_ sender: UISlider) {
+        let weight = String(format:"%.0f", sender.value)
+        weightLabel.text = "\(weight)Kg"
+        
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let bmi = weight / pow(height,2)
+        
+        let secondView = SecondViewController()
+        secondView.bmiValue = String(format:"%.1f",bmi)
+        
+        self.present(secondView, animated:true, completion:nil)
+    }
 }
 
---
+++ ResultsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ResultsViewController.swift
+//  BMI Calculator
+//
+//  Created by Jhonatan Fernando on 2020-04-21.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
